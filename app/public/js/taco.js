0654f15fa7d26c2ee2e6d2d3fb4a8e68fb73ed3c
Matthew Kinzle <matt.kinzle@gmail.com>
2020-07-23 18:56:55 -0500

    added get route

--- app/public/js/taco.js
+++ app/public/js/taco.js
@@ -5,6 +5,7 @@ $("#taco-submit").on("click", function (event) {
     let newTaco = {
         taco_name: $("#tacotext").val().trim(),
     };
+    console.log(newTaco);
 
     $.post("/api/new", newTaco)
         // On success, run the following code
@@ -24,8 +25,30 @@ $("#taco-submit").on("click", function (event) {
     // Empty each input box by replacing the value with an empty string
     $("#tacotext").val("");
 
+});
+
+$.get("/api/all", function (data) {
+
+    if (data.length !== 0) {
+
+        for (var i = 0; i < data.length; i++) {
+
+            let row = $("<li>");
+            row.addClass("list-group-item");
+            row.text(data.taco_name)
+
+            $("<button>").attr({ class: "btn submit-btn", type: "submit" }).text("Down the Hatch!")
+            row.append(".submit-btn")
+
+
+            $("#to-eat").append(row);
+
+        }
+
+    }
 
 });
+
 
 
 
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,5 +1,33 @@
 const Taco = require("../models/taco.js");
 
 module.exports = function (app) {
-    app.get("/api/add")
+
+    app.get("/api/all", function (req, res) {
+
+        // Finding all Chirps, and then returning them to the user as JSON.
+        // Sequelize queries are asynchronous, which helps with perceived speed.
+        // If we want something to be guaranteed to happen after the query, we'll use
+        // the .then function
+        Taco.findAll({}).then(function (results) {
+            // results are available to us inside the .then
+            res.json(results);
+        });
+
+    });
+
+    app.post("/api/new", function (req, res) {
+
+        console.log("Taco Data:");
+        console.log(req.body);
+
+        Taco.create({
+            taco_name: req.body.taco_name,
+
+        }).then(function (results) {
+            // `results` here would be the newly created chirp
+            res.end();
+        });
+
+    });
+
 }
