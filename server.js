b6a8895484e47a10e823b1453fe1c9af2a791479
Matthew Kinzle <matt.kinzle@gmail.com>
2020-07-27 16:18:13 -0500

    sync sequelize

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const db = require("./app/models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,29 @@ require("./app/routes/api-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync().then(() => {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
 });
+
+// const express = require("express");
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static directory
+// app.use(express.static("app/public"));
+
+// // Routes
+// require("./app/routes/api-routes.js")(app);
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
